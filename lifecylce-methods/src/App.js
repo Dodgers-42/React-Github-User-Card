49dd8d320270210dcc2f303f94d87203daf08768
Dodger-42 <kevin.bing.barber@gmail.com>
2020-08-06 19:39:10 -0700

    second

--- lifecylce-methods/src/App.js
+++ lifecylce-methods/src/App.js
@@ -2,26 +2,83 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  // make App class component like the lecture
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    // console.log("Constructor");
+    super();
+    this.state = {
+      user: [],
+      userCard: ""
+    };
+  }
 
-export default App;
+  // componentDidMount() {
+  //   // console.log("CDM running");
+  //   axios
+  //     .get("https://api.github.com/users/Dodgers-42")
+  //     .then((res) => {
+  //       this.setState({ user: res.data });
+  //       // console.log(this.state);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }
+
+  // componentDidUpdate(prevState, prevProps) {
+  //   if (prevState.doggos !== this.state.doggos) {
+  //     console.log("doggos have changed!");
+  //   }
+  //   if (prevState.dogBreed !== this.state.dogBreed) {
+  //     console.log("State updated, dog breed:", this.state.dogBreed);
+  //   }
+  // }
+
+  fetchUsers = () => {
+    axios
+      .get(`https://api.github.com/users/Dodgers-42`)
+      .then((res) => {
+        this.setState({ user: res.data });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  handleChanges = (e) => {
+    console.log("handleChanges called");
+    this.setState({
+      // take the previous state, and just change the dogBreed text
+      ...this.state,
+      dogBreed: e.target.value
+    });
+  };
+
+  render() {
+    console.log("Render");
+    return (
+      
+    );
+  }
+}
+  export default App;
+
+// function App() {
+//   // make App class component like the lecture
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
