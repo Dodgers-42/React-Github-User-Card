b6ba27b3ad49eed357f7976a1ef5796f565ef2b2
Dodger-42 <kevin.bing.barber@gmail.com>
2020-08-10 16:32:32 -0700

    yep

--- lifecylce-methods/src/App.js
+++ lifecylce-methods/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import Card from './Card';
@@ -9,8 +9,10 @@ class App extends React.Component {
     // console.log("Constructor");
     super();
     this.state = {
-      user: [],
-      userCard: ""
+      
+      user: {},
+      following: [],
+      
     };
   }
 
@@ -19,8 +21,8 @@ class App extends React.Component {
     axios
       .get("https://api.github.com/users/Dodgers-42")
       .then((res) => {
-        this.setState({ user: res.data });
-        console.log("got it",this.state);
+        this.setState({ user: res.data, ...this.state });
+        // console.log("got it",this.state);
       })
       .catch((err) => console.log("Damn", err));
   }
@@ -28,19 +30,21 @@ class App extends React.Component {
   componentDidUpdate(prevState, prevProps) {
     console.log("updating");
     if (prevState.user !== this.state.user) {
-      console.log("user have changed!");
+      console.log("user have changed!", pre);
+    this.fetchFallowing();
     }
-    if (prevState.userCard !== this.state.userCard) {
-      console.log("State updated, user card:", this.state.userCard);
+    if (prevState.following !== this.state.following) {
+      console.log("State updated, user card:", this.state.following);
     }
   }
 
-  fetchUsers = () => {
+  fetchFallowing = () => {
+    console.log("fetching")
     axios
-      .get(`https://api.github.com/users/Dodgers-42/following`)
+      .get(`https://api.github.com/users/Dodgers-42/followers`)
       .then((res) => {
-        this.setState({ following: res.data });
-         console.log(this.state.following) 
+        //this.setState({ following: res.data.following, ...this.state });
+  
       })
       .catch((err) => console.log('following',err));
   };
@@ -48,22 +52,53 @@ class App extends React.Component {
   // handleChanges = (e) => {
   //   console.log("handleChanges called");
   //   this.setState({
-  //     // take the previous state, and just change the userCard text
+  //     // take the previous state, and just change the fallowing text
   //     ...this.state,
-  //     userCard: e.target.value
+  //     following: e.target.value
   //   });
   // };
 
   render() {
-    console.log("Render");
+    console.log("Render", this.state);
     return (
-      <div>
+      <div className= "App">
         <Card user={this.state.user} following={this.state.following} />
-      </div>
-      // <h1>Name List</h1>
       
+       <h1>Name List</h1>
+
+      {/* <input
+          type="text"
+          value={this.state.following}
+          onChange={this.handleChanges}
+        />
+        <button onClick={this.fetchUser}>Fetch User</button>
+        <div className="user">
+          {this.state.usewr.map((user) => (
+            <img width="150" className="dog" key={user} src={user} />
+          ))}
+          ;
+      </div> */}
+      </div>
     );
   }
 }
+
+// {users.map(user => (
+//   <div key={user.id} className="card">
+//       <img src={user.avatar_url} alt='profile avatar' />
+//       <div className="card-info">
+//           <h3 className="name">{user.name}</h3>
+//           <p className="username">{user.login}</p>
+//           <p>Location: {users.location}</p>
+//           <p>Profile:
+//               <a href={user.url}>{user.url}</a>
+//           </p>
+//           <p>Followers: {user.followers}</p>
+//           <p>Following: {users.following}</p>
+//           <p>Bio: {user.bio}</p>
+//       </div>
+//   </div>
+// ))}
+
   export default App;
 
