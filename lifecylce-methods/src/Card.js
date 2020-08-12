95a31a681458b64dda617e2583ec38a9cbe1c91d
Dodger-42 <kevin.bing.barber@gmail.com>
2020-08-11 18:39:35 -0700

    done

--- lifecylce-methods/src/Card.js
+++ lifecylce-methods/src/Card.js
@@ -4,33 +4,52 @@ class Card extends React.Component {
     constructor(){
         super();
         this.state = {
-            users: [],
+            user: [],
         };
     }
     componentDidMount() {
         console.log(this.props) //gives user and follower, but both are empty
     }
     render(){
+        console.log("this is it",this.props);
         return(
-            <div></div>
+            <div>
+            {this.props.following.map(user => (
+      <div className="card" key={user.id} >
+          <img src={user.avatar_url} alt='profile avatar' />
+          <div className="card-info">
+              <h3 className="name">{user.name}</h3>
+              <p className="username">{user.login}</p>
+              <p>Location: {user.location}</p>
+              <p>Profile:
+                  <a href={user.url}>{user.url}</a>
+              </p>
+              <p>Followers: {user.followers}</p>
+              <p>Following: {user.following}</p>
+              <p>Bio: {user.bio}</p>
+          </div>
+      </div>
+      
+    ))}
+    <div className="card" key={this.props.user.id} >
+          <img src={this.props.user.avatar_url} alt='profile avatar' />
+          <div className="card-info">
+              <h3 className="name">{this.props.user.name}</h3>
+              <p className="username">{this.props.user.login}</p>
+              <p>Location: {this.props.user.location}</p>
+              <p>Profile:
+                  <a href={this.props.user.url}>{this.props.user.url}</a>
+              </p>
+              <p>Followers: {this.props.user.followers}</p>
+              <p>Following: {this.props.user.following}</p>
+              <p>Bio: {this.props.user.bio}</p>
+          </div>
+      </div>
+    </div>
+    
         )
     }
 }
 
-//{users.map(user => (
-    //   <div key={user.id} className="card">
-    //       <img src={user.avatar_url} alt='profile avatar' />
-    //       <div className="card-info">
-    //           <h3 className="name">{user.name}</h3>
-    //           <p className="username">{user.login}</p>
-    //           <p>Location: {users.location}</p>
-    //           <p>Profile:
-    //               <a href={user.url}>{user.url}</a>
-    //           </p>
-    //           <p>Followers: {user.followers}</p>
-    //           <p>Following: {users.following}</p>
-    //           <p>Bio: {user.bio}</p>
-    //       </div>
-    //   </div>
-    // ))}
+
 export default Card;
